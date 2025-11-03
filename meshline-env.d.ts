9fe2a64f53c9b72e6c042993dda3f08a9b7a0c0e
yusufproject01 <yusufproject01@gmail.com>
2025-11-03 16:40:34 +0700

    Selesai

--- meshline-env.d.ts
+++ meshline-env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="@react-three/fiber" />
 
-import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
-import { Object3DNode } from '@react-three/fiber'
+import { MeshLineGeometry, MeshLineMaterial } from "meshline";
+import { Object3DNode } from "@react-three/fiber";
 
-declare module '@react-three/fiber' {
+declare module "@react-three/fiber" {
   interface ThreeElements {
-    meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>
-    meshLineMaterial: Object3DNode<MeshLineMaterial, typeof MeshLineMaterial>
+    meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+    meshLineMaterial: Object3DNode<MeshLineMaterial, typeof MeshLineMaterial>;
   }
 }
