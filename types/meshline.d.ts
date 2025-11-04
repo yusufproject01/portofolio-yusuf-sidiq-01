a4b2d8f0641390d0a10166b0b6a65b99bb39f06a
yusufproject01 <yusufproject01@gmail.com>
2025-11-04 09:49:38 +0700

    Selesai

---
+++ types/meshline.d.ts
@@ -0,0 +1,11 @@
+import { MeshLineGeometry, MeshLineMaterial } from "meshline";
+import { Object3DNode } from "@react-three/fiber";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+      meshLineMaterial: Object3DNode<MeshLineMaterial, typeof MeshLineMaterial>;
+    }
+  }
+}
